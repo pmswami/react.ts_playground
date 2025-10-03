c4dea4332fe4cdbe70386dd582185aeeeb5d10f0
unknown <usern5949@gmail.com>
2025-10-04 01:33:22 +0530

    Added event handlers

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,32 +1,34 @@
 
 import './App.css';
+import { Button } from './components/Button';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
 
 function App() {
-  const personName={
-    first:"Bruce",
-    last: "Wayne"
-  }
+  // const personName={
+  //   first:"Bruce",
+  //   last: "Wayne"
+  // }
 
-  const nameList = [
-    {
-      first:"Bruce",
-      last:"Wayne"
-    },
-    {
-      first:"Clark",
-      last:"Kent"
-    },
-    {
-      first:"Princess",
-      last:"Diana"
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     first:"Bruce",
+  //     last:"Wayne"
+  //   },
+  //   {
+  //     first:"Clark",
+  //     last:"Kent"
+  //   },
+  //   {
+  //     first:"Princess",
+  //     last:"Diana"
+  //   }
+  // ]
   return (
     <div className="App">
       {/* <Greet name="Swamfire" messageCount={20} isLoggedIn={false} /> */}
@@ -34,12 +36,16 @@ function App() {
       {/* <PersonList names={nameList}/> */}
       {/* <Status status="loading"/> */}
       {/* <Status status="error"/> */}
-      <Status status="success"/>  
-      <Heading>Placeholder Text</Heading>
-      <Oscar><Heading>Oscar Goes to Decaprio</Heading></Oscar>
-      <Greet name='Swam' isLoggedIn={true}/>
+      {/* <Status status="success"/>   */}
+      {/* <Heading>Placeholder Text</Heading> */}
+      {/* <Oscar><Heading>Oscar Goes to Decaprio</Heading></Oscar> */}
+      {/* <Greet name='Swam' isLoggedIn={true}/> */}
+      <Button handleClick={(event, id)=>{
+        console.log("Button Clicked", event,id)
+      }}/>
+      <Input value="" handleChange={(event)=>console.log(event)}/>
     </div>
-    
+     
   );
 }
 
